a3104d4acada9d86d57ff0583baf4d3a87765301
Jorge Guepud <jlguepudb@unal.edu.co>
2023-09-14 17:00:02 -0500

    :sparkles: feat: Se agregan las rutas hijas como lazyLoad

--- src/app/countries/countries-routing.module.ts
+++ src/app/countries/countries-routing.module.ts
@@ -9,9 +9,7 @@ const routes: Routes = [
     { path: 'by-capital', component: ByCapitalPageComponent },
     { path: 'by-country', component: ByCountryPageComponent },
     { path: 'by-region', component: ByRegionPageComponent },
-    { path: 'by-region', component: ByRegionPageComponent },
     { path: 'by/:id', component: CountryPageComponent },
-
 ]
 
 @NgModule({
