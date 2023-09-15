31918c38b2af4c69797564a6bd2715bfcc644a2e
Jorge Guepud <jlguepudb@unal.edu.co>
2023-09-14 19:43:17 -0500

    :sparkles: feat: Se agrega el compontente searchbox y evento

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,12 +7,14 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 @NgModule({
   declarations: [
     AboutPageComponent,
     ContactPageComponent,
     HomePageComponent,
+    SearchBoxComponent,
     SidebarComponent,
   ],
   imports: [
@@ -23,6 +25,7 @@ import { ContactPageComponent } from './pages/contact-page/contact-page.componen
     AboutPageComponent,
     ContactPageComponent,
     HomePageComponent,
+    SearchBoxComponent,
     SidebarComponent,
   ]
 })
