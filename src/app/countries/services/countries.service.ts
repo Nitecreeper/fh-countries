07c9452f2138bcfe88d7bd2fdbf7783a568c303b
Jorge Guepud <jlguepudb@unal.edu.co>
2023-09-14 20:25:16 -0500

    :sparkles: feat: Se agrega component countryTable, service countries e interface country

---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country.interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountriesService {
+
+  private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  public searchCapital (term: string): Observable<Country[]>{
+    const url = `${this.apiUrl}/capital/${term}`
+    return this.httpClient.get<Country[]>(url);
+  }
+
+
+}
